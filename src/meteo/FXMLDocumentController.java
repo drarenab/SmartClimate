1702af044aaddc8832bfd41ce028aa8ce4af681a
karim <abdelkarim.drareni@hotmail.com>
2017-01-28 21:12:43 +0100

    Controlleur commun a plusieurs interface + interface Siting

--- src/meteo/FXMLDocumentController.java
+++ src/meteo/FXMLDocumentController.java
@@ -13,15 +13,30 @@ import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.event.EventType;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.BackgroundSize;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 /**
  *
@@ -37,18 +52,71 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     Text text, text2;
     @FXML
-    VBox v;
+    VBox v,VboxPrincipal;
     @FXML
     GridPane gridPane;
     Coordonne emplacement;
-
+    static int Interface;
     @FXML
-    private void handleButtonAction(ActionEvent event) {
+    MenuBar menuBar;
+    @FXML
+    AnchorPane anchorSeting;
+    
+    @FXML
+    private void handleButtonActionPreference() throws IOException {
+        
     }
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        /*Commun a toutes les interface */
+        
+        /*creation d'un menu dynamiquement commun a toutes les interfaces*/
+        menuBar=new MenuBar();
+        Menu file =new Menu("File");
+        
+        Menu edit =new Menu("Edit");
+        Menu window =new Menu("Window");
+        Menu seting =new Menu("Seting");
+        MenuItem preference=new MenuItem("Preférence");
+        preference.setOnAction(new EventHandler<ActionEvent>(){
+        
+
+            @Override
+            public void handle(ActionEvent event) {
+                Interface=1;
+                Parent root;
+                try {
+                    root = FXMLLoader.load(getClass().getResource("fxmlSetting.fxml"));
+                     Scene scene = new Scene(root);
+                     Stage s=new Stage();
+                     s.setScene(scene);
+                     s.show();
+                    
+                } catch (IOException ex) {
+                    Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+                }
+        
+       
+            }
+                });
+        seting.getItems().add(preference);
+        
+        menuBar.getMenus().addAll(file,edit,window,seting);
+        
+        
+        VboxPrincipal.getChildren().add(0, menuBar);
+        if (Interface==0){
+            InitInterfacePrincipal();
+        }
+        else{
+            initInterfaceSetting();
+        }
+    }
+
+    public void InitInterfacePrincipal(){
+        Interface=0;
         /*recuperation de la l'image de la carte de france*/
         image = new Image(Meteo.class.getResourceAsStream("Image/country-fra.png"));
         /*instanciation de la liste des villes*/
@@ -71,12 +139,19 @@ public class FXMLDocumentController implements Initializable {
         
         try {
             /* Essai telechargement et decompression */
-            Downloader.downLoadCsv();
+           Downloader.downLoadCsv();
         } catch (IOException ex) {
             Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
         }
-        Downloader.DecompresserGzip();
-
+        try {
+            Downloader.DecompresserGzip();
+        } catch (Exception e) {
+        }
+        Interface=1;
     }
-
+    public void initInterfaceSetting(){
+    Image img=new Image(Meteo.class.getResourceAsStream("Image/BackgroundSetting2.jpg"));
+        BackgroundImage background =new BackgroundImage(img, BackgroundRepeat.REPEAT, BackgroundRepeat.REPEAT, BackgroundPosition.CENTER, BackgroundSize.DEFAULT);
+        anchorSeting.setBackground(new Background(background));
+    }
 }
